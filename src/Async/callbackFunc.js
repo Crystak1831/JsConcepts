833c25d95c10ff731f5692bf99af1ed1f2cd90d1
Crystal_1831 <43097573+Crystak1831@users.noreply.github.com>
2021-09-18 00:02:36 -0400

    change number

--- src/Async/callbackFunc.js
+++ src/Async/callbackFunc.js
@@ -1,3 +1,16 @@
 // callback function is a function pass an arguement to another function
 
 // ex1:
+function CountNum(callBack, arr) {
+  let output = [];
+  for (let i = 0; i < arr.length; i++) {
+    output.push(callBack(arr[i]));
+  }
+  return output;
+}
+
+function doubleNum(num) {
+  return num * 2;
+}
+
+console.log(CountNum(doubleNum, [1, 2, 3]));
