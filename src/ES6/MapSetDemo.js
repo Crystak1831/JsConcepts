f917e3561f3ccb2a6ed36bd1bc1416af03a9bd1a
Crystal_1831 <43097573+Crystak1831@users.noreply.github.com>
2021-09-22 17:43:25 -0400

    add Set

--- src/ES6/MapSetDemo.js
+++ src/ES6/MapSetDemo.js
@@ -1,16 +1,16 @@
 // Map demo:
-let myMap = new Map();
-myMap.set({}, "hello");
-myMap.get({}); //udefined 取不出值，因为用obj的话,reference不一样
+// let myMap = new Map();
+// myMap.set({}, "hello");
+// myMap.get({}); //udefined 取不出值，因为用obj的话,reference不一样
 // console.log(myMap.get({}))
 
 //改正：定义一个obj，是的{}指向obj
-let myMap1 = new Map();
-let objKey = {};
+// let myMap1 = new Map();
+// let objKey = {};
 //function也是一样的：
-let funcKey = function () {};
-myMap1.set(objKey, "Hello:objKey");
-myMap1.set(funcKey, "Hello:Funckey");
+// let funcKey = function () {};
+// myMap1.set(objKey, "Hello:objKey");
+// myMap1.set(funcKey, "Hello:Funckey");
 // console.log(myMap1.get(objKey))
 // console.log(myMap1.get(funcKey))
 
@@ -26,15 +26,23 @@ myMap1.set(funcKey, "Hello:Funckey");
 
 //  loop a map:只能用for of
 //loop key:
-for (let key of myMap1.keys()) {
-  console.log(key, "key");
-}
-//loop value:
-for (let value of myMap1.values()) {
-  console.log(value, "values");
-}
+// for (let key of myMap1.keys()) {
+//   console.log(key, "key");
+// }
+// //loop value:
+// for (let value of myMap1.values()) {
+//   console.log(value, "values");
+// }
 
-//loop entries
-for (let [key, value] of myMap1.entries()) {
-  console.log(key, value, "pair");
-}
+// //loop entries
+// for (let [key, value] of myMap1.entries()) {
+//   console.log(key, value, "pair");
+// }
+
+// 2.Set：array like
+let arr = [1, 2, 4, 5, 3, 3, 5];
+let mySet = new Set(arr);
+mySet.add("hello");
+console.log(mySet);
+//Set 转化为 array 用Array.from
+console.log(Array.from(mySet));
